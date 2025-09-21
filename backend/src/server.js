3bc5c4e759e2195c732e459e5b66546b14f02c0b
Md Noorullah Raza <mnraza94@gmail.com>
2025-09-21 23:03:05 +0530

    add a server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,9 @@
 import express from "express"
+import dotenv from "dotenv"
 
 const app = express()
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-    console.log("server is  running ");
-
+dotenv.config() 
+const port = process.env.PORT || 5000  
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
 })
