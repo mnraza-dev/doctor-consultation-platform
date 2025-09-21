b4f13ebba675955c2e333c355dc62bd27c96ca73
Md Noorullah Raza <mnraza94@gmail.com>
2025-09-21 23:55:02 +0530

    add HeaderProps for Header

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,23 @@
 import Header from '@/components/landing/Header'
-import React from 'react'
+import { useRouter } from 'next/navigation'
+import React, { useEffect } from 'react'
 
 const Home = () => {
+  const user = {
+    type: 'pa'
+  }
+  const router = useRouter();
+  useEffect(() => {
+    if (user.type === 'doctor') {
+      router.replace('/doctor/dashboard')
+    }
+  }, [user, router])
+
+  if (user?.type === 'doctor') {
+    return null
+  }
   return (
     <div className='min-h-screen bg-white'>
-
       <Header showDashboardNavbar={false} />
     </div>
   )
--- frontend/src/components/landing/Header.tsx
+++ frontend/src/components/landing/Header.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+interface HeaderProps {
+  showDashboardNavbar?: boolean,
 
-const Header = ({showDashboardNavbar}) => {
+}
+interface NavigationItem {
+  label: string;
+  icon: React.ComponentType<any>;
+  href: string;
+  active: boolean;
+
+}
+const Header: React.FC<HeaderProps> = ({ showDashboardNavbar }) => {
   return (
     <div className='text-black'>
       {showDashboardNavbar &&
-      <h1>Navbar Header</h1>
+        <h1>Navbar Header</h1>
       }
     </div>
   )
