26ac91b62880480db84fdd3ce92aac6abf5230d7
Md Noorullah Raza <mnraza94@gmail.com>
2025-09-21 23:16:38 +0530

    add helmet and morgan

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,19 @@
 import express from "express"
 import dotenv from "dotenv"
-
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import bodyParser from "body-parser";
 const app = express()
-dotenv.config() 
-const port = process.env.PORT || 5000  
+dotenv.config()
+
+app.use(helmet())
+app.use(morgan('dev'))
+app.use(cors({
+  credentials: true,
+  origin: process.env.ALLOWED_ORIGIN
+}))
+const port = process.env.PORT || 5000
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
 })
